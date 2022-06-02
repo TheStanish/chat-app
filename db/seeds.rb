f6e47d7736a434d16643ec097c78e2104c3f5cf6
TheStanish <tstan2310@gmail.com>
2022-06-02 12:35:53 -0400

    generated User model and table, ran new migration, used Hirb gem to add tabular tables within rails console

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Bibi", password: "password")
+User.create(username: "Ricky", password: "password")
+User.create(username: "John", password: "password")
+User.create(username: "Jane", password: "password")
+User.create(username: "Mashrur", password: "password")
